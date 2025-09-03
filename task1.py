be52cdbee2401d728b1859f0f1490063cf89e09c
Uttam Shukla <utshukla553@gamil.com>
2025-09-03 08:20:16 +0530

    completed assingment2

---
+++ task1.py
@@ -0,0 +1,5 @@
+number =int(input('Enert an interger :'))
+if number %2==0:
+    print ('the number is even :')
+else:
+    print ('thr number is odd :')
---
+++ task2.py
@@ -0,0 +1,4 @@
+total_sum =0
+for i in range(1,51):
+ total_sum += i
+print('The sum of number from 1to 50 is:', total_sum)
